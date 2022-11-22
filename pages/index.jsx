8e02479862d87efe94c0d39b5abe1a13dd36f86a
k-taro-reccos <tarou3806@gmail.com>
2022-11-22 12:23:57 +0900

    ✨

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import { Layout } from "components/Layout";
+import { Auth } from "components/Auth";
+
+const Home = () => {
+  return (
+    <Layout title="Login">
+      <Auth />
+    </Layout>
+  );
+};
+
+export default Home;
